d43c2ba3cdc4366d7e2c6d9dca93c893ca64faf6
Obaloluwa Olusoji <olusojiobah@gmail.com>
2023-02-27 01:02:18 +0100

    fixed header

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,57 @@
+import { useState } from "react";
 import "../styles/header.css";
+import { NavLink } from "react-router-dom";
 
-export default function Header() {
+function Header() {
+  const [address, setAddress] = useState("connect");
+
+  const navItems = [
+    { name: "Dashboard", href: "/dashboard" },
+    { name: "Market", href: "/market" },
+    { name: "Pool", href: "/pool" },
+    { name: "Stake", href: "/stake" },
+  ];
+
+  // TODO Do a function to get wallet address after connect and set it as address
+  // TODO Do a functionality to truncate the address string
+  const handleConnect = () => {
+    if (address === "connect") {
+      setAddress("0x79B0…4E71");
+    } else {
+      setAddress("connect");
+    }
+  };
+
+  let activeStyle = {
+    color: "#ffffff",
+    textDecoration: "underline",
+  };
+
   return (
     <>
-      <div className="top-section">
-        <a href="/">
-          <h1 className="header">InterXchange</h1>
-        </a>
-        <button className="connect-btn">0x79B0…4E71</button>
+      <div className="top">
+        <a href="/">InterXchange</a>
+        <button onClick={handleConnect}>{address}</button>
       </div>
-      <nav>
-        <ul className="menu">
-          <li className="menu-item">
-            <a href="/dashboard">Dashboard</a>
-          </li>
-          <li className="menu-item">
-            <a href="/market">Market</a>
-          </li>
-          <li className="menu-item">
-            <a href="/pool">Pool</a>
-          </li>
-          <li className="menu-item">
-            <a href="/stake">Stake</a>
-          </li>
-        </ul>
-      </nav>
+      <div>
+        <nav>
+          <ul className="menu">
+            {navItems.map((item) => (
+              <li className="menuItems">
+                <NavLink
+                  to={item.href}
+                  key={item.name}
+                  style={({ isActive }) => (isActive ? activeStyle : undefined)}
+                >
+                  {item.name}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </>
   );
 }
+
+export default Header;
